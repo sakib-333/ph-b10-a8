60e4c0772e3771d0afa4abd8c3d8771d5bdf22b5
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-05 19:08:13 +0600

    Show all gadgets of wishlist.

--- src/components/Body/BodyComponents/Wishlist.jsx
+++ src/components/Body/BodyComponents/Wishlist.jsx
@@ -1,33 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import CancelIcon from "/icons/cancel-icon.svg";
+import { GadgetHavenContext } from "../../context/GadgetHavenContext";
 
 const Wishlist = () => {
+  const { gadgets, wishlist } = useContext(GadgetHavenContext);
+  const finalWishlist = gadgets.filter((gadget) =>
+    wishlist.includes(gadget.product_id)
+  );
   return (
     <div className="my-5 p-3">
       <div className="flex flex-col md:flex-row items-center justify-between">
         <h1 className="font-bold">WishList</h1>
       </div>
-      <div className="my-4">
-        <div className="bg-white px-4 flex flex-col md:flex-row items-center justify-between rounded-xl overflow-hidden">
-          <div className="w-full flex flex-col md:flex-row items-center">
-            <img
-              className="w-40 h-40"
-              src="https://i.postimg.cc/wjGTSXbv/asus-1.jpg"
-              alt=""
-            />
-            <div>
-              <h1 className="text-xl font-bold">Samsung Galaxy S23 Ultra</h1>
-              <p className="text-gray-500">
-                Ultra-slim, high-performance laptop with 13.4-inch Infinity Edge
-                display.
-              </p>
-              <strong>Price: $ 999.99</strong>
+      <div className="my-4 space-y-3">
+        {finalWishlist.map((gadget) => (
+          <div
+            key={gadget.product_id}
+            className="bg-white px-4 flex flex-col md:flex-row items-center justify-between rounded-xl overflow-hidden border"
+          >
+            <div className="w-full flex flex-col md:flex-row items-center">
+              <img
+                className="w-40 h-40"
+                src={gadget.product_image}
+                alt={gadget.product_title}
+              />
+              <div>
+                <h1 className="text-xl font-bold">{gadget.product_title}</h1>
+                <p className="text-gray-500">{gadget.description}</p>
+                <strong>Price: $ {gadget.price}</strong>
+              </div>
             </div>
+            <button className="hover:opacity-50">
+              <img src={CancelIcon} alt="cancel icon" />
+            </button>
           </div>
-          <button className="hover:opacity-50">
-            <img src={CancelIcon} alt="cancel icon" />
-          </button>
-        </div>
+        ))}
       </div>
     </div>
   );
